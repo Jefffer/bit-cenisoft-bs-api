83d2d218f79fe57bc66d08443bf3d3a97c4b36f6
Jefferson Rodriguez <jefferson.rodriguez@exsis.com.co>
2021-02-17 17:44:24 -0500

    Sales and Details model creation.

--- components/sale/model.js
+++ components/sale/model.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose')
 
+// Child model
+const detailSchema = new mongoose.Schema({
+  bookId: {type: Schema.Types.ObjectId, ref: "books"},
+  bookName: { type: String, required: true },
+  unitValue: { type: Number, required: true, min: 0 },
+  amount: { type: Number, required: true, min: 0 }
+})
+
+// Parent model
 const saleSchema = new mongoose.Schema({
   date: { type: String, required: true },
   total: { type: String, required: true },
-  clientId: {type: Schema.Types.ObjectId, ref: "clients"}
-  
+  clientId: {type: Schema.Types.ObjectId, ref: "clients"},
+  details: [detailSchema]
 })
 
 const Sale = mongoose.model('sales', saleSchema)
